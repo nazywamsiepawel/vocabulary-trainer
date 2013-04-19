b739e574ececeb0757738b2e1e8f640a32bc2d00
Pawel Borkowski <borkowskip@gmail.com>
2013-04-19 08:34:37 +0100

    .

--- js/chart.js
+++ js/chart.js
@@ -1,58 +1,64 @@
-
-var canvasChart = function(options){
-	this.options = options || false; 
-	this.chartData = [];
+var canvasChart = function (options) {
+    this.options = options || false;
+    this.chartData = [];
 }
 
 canvasChart.prototype = {
-	addData : function(data){
-		this.chartData.push(data);
-	},
+    addData: function (data) {
+        for (var i = 0; i < data.length; i++) {
+            this.chartData.push(data[i]);
+        }
+    },
 
-	render : function(){
+    render: function () {
 
-	  var canvasId = this.options.canvasId;
-	  var height = this.options.height;
-	  var width = this.options.width;
-	  var posX = this.options.posX;
-	  var posY = this.options.posY;
-	  var max = this.options.max;
+        var canvasId = this.options.canvasId;
+        var height = this.options.height;
+        var width = this.options.width;
+        var posX = this.options.posX;
+        var posY = this.options.posY;
+        var max = this.options.max;
 
-	  var canvasBlock = document.getElementById(canvasId);
-   	  var canvas = canvasBlock.getContext('2d');
-   	  var x_unitSize = Math.floor(width/this.chartData.length)/2;
-   	  
+        var canvasBlock = document.getElementById(canvasId);
+        var canvas = canvasBlock.getContext('2d');
+        /*clear canvas*/
 
+        canvas.save();
+        canvas.setTransform(1, 0, 0, 1, 0, 0);
+        canvas.clearRect(0, 0, canvasBlock.width, canvasBlock.height);
+        canvas.restore();
 
-   	  var renderData = [];
-   	  /* 1. calculate positions */
-   	  for(var i=0; i<this.chartData.length; i++){
-   	  	var dataRecord = this.chartData[i];
-   	  	renderData.push({x:dataRecord.x*x_unitSize, y:height-(dataRecord.y*height)});
-   	  }
+        var x_unitSize = Math.floor(width / this.chartData.length) / 2;
 
-   	  console.log(renderData);
+        var renderData = [];
+        /* 1. calculate positions */
+        for (var i = 0; i < this.chartData.length; i++) {
+            var dataRecord = this.chartData[i];
+            renderData.push({
+                x: dataRecord.x * x_unitSize,
+                y: height - (dataRecord.y * height)
+            });
+        }
+        /* 2. render on canvas */
 
-   	  /* 2. render on canvas */
+        for (var i = 0; i < renderData.length; i++) { //points
+            canvas.beginPath();
+            canvas.arc(renderData[i].x, renderData[i].y, 3, 0, 2 * Math.PI, false);
+            canvas.lineWidth = 3;
+            canvas.strokeStyle = '#dedede';
+            canvas.stroke();
+            canvas.fillStyle = "#dedede";
+            canvas.fill();
+            canvas.closePath();
+        }
 
-   	  for(var i=0; i<renderData.length; i++){ //points
-   	    canvas.beginPath();
-   	  	canvas.arc(renderData[i].x, renderData[i].y, 3, 0, 2 * Math.PI, false);
-       	canvas.lineWidth = 3;
-      	canvas.strokeStyle = '#dedede';
-      	canvas.stroke();
-      	canvas.fillStyle = "#dedede";
-        canvas.fill();
-        canvas.closePath();
-   	  }
-
-   	  for(var i=0; renderData.length; i++){
-   	  	canvas.beginPath();
-	    	canvas.moveTo(renderData[i].x,renderData[i].y);
-		    canvas.lineTo(renderData[i+1].x,renderData[i+1].y);
-		    canvas.lineWidth = 3;
-      	canvas.strokeStyle = '#dedede';
-		    canvas.stroke();
-   	  }
-	}
+        for (var i = 0; i<renderData.length-1; i++) { //lines
+            canvas.beginPath();
+            canvas.moveTo(renderData[i].x, renderData[i].y);
+            canvas.lineTo(renderData[i + 1].x, renderData[i + 1].y);
+            canvas.lineWidth = 3;
+            canvas.strokeStyle = '#dedede';
+            canvas.stroke();
+        }
+    }
 }
--- js/trainer.js
+++ js/trainer.js
@@ -4,122 +4,156 @@
 		- periodic rerenders of canvas chart
 		- selecting different files
 */
+var VocabularyTrainer = function (options, vocabularyData) {
+    this.options = options;
+    this.vocabularyData = vocabularyData;
+    this.currentShuffled = [];
+    this.stats = [];
 
+    for (var i = 0; i < options.trainingCycles; i++) {
+        this.stats.push(new Array());
+    }
 
+    var i = 0;
 
-var VocabularyTrainer = function(options, vocabularyData){
-	this.options = options;
-	this.vocabularyData = vocabularyData;
-	this.currentShuffled = [];
-	this.stats = [];
-	
-	for(var i=0; i<options.trainingCycles; i++){
-		this.stats.push(new Array());
-	}
+    for (var i = 0; i < this.vocabularyData.words.length; i++) {
+        var word = this.vocabularyData.words[i];
+        word.id = i;
+        this.currentShuffled.push(word);
+    }
 
-	var i = 0;
-
-	for(var i=0; i<this.vocabularyData.words.length; i++){
-		var word = this.vocabularyData.words[i];
-		word.id = i;
-		this.currentShuffled.push(word);
-	}
-
-	this.shuffleNewBatch();
-	this.trainingCycles = options.trainingCycles || 5;
-	this.context = {currentWordIndex:0, currentWordCount:0, currentCycle:0};
+    this.shuffleNewBatch();
+    this.trainingCycles = options.trainingCycles || 5;
+    this.context = {
+        currentWordIndex: 0,
+        currentWordCount: 0,
+        currentCycle: 0
+    };
 }
 
 VocabularyTrainer.prototype = {
-	loadJSON : function(fileName){
-		/*load up from file*/
-	},
+    loadJSON: function (fileName) {
+        /*load up from file*/
+    },
 
-	intialize : function(context){
-		this.context.currentCycle = context.currentCycle || 0;
-		this.context.currentWordIndex = context.currentWordIndex || 0;
+    intialize: function (context) {
+        this.context.currentCycle = context.currentCycle || 0;
+        this.context.currentWordIndex = context.currentWordIndex || 0;
 
-	},
+    },
 
-	generatePossibleLetters : function(){
-		var letterFreq = {};
+    generatePossibleLetters: function () {
+        var letterFreq = {};
 
-		for(var j=0; j<this.vocabularyData.words.length; j++){
-		     var w = this.vocabularyData.words[j].translation;
-		     for(var i=0; i<w.length; i++)
-		       if(_.isUndefined(letterFreq[w[i]])) 
-		       		letterFreq[w[i]]=0;
-		       else 
-		       		letterFreq[w[i]]++;
-		}
-		return letterFreq;
-	},
+        for (var j = 0; j < this.vocabularyData.words.length; j++) {
+            var w = this.vocabularyData.words[j].translation;
+            for (var i = 0; i < w.length; i++)
+                if (_.isUndefined(letterFreq[w[i]]))
+                    letterFreq[w[i]] = 0;
+                else
+                    letterFreq[w[i]]++;
+        }
+        return letterFreq;
+    },
 
-	shuffleNewBatch : function(){
-		this.currentShuffled = shuffle(this.currentShuffled);
-		console.log(this.currentShuffled);
-	},
+    shuffleNewBatch: function () {
+        this.currentShuffled = shuffle(this.currentShuffled);
+        console.log('Shuffled batch : ');
+        console.log(this.currentShuffled);
+    },
 
-	submitAnswer : function(user_translation, word_id){
-		var t_translation = this.vocabularyData.words[this.context.currentWordIndex].translation;
-		if(t_translation == user_translation){
-			console.log("true");
-			this.addRecord(this.context.currentWordIndex, true);
-			return {status:true}
-		} else {
-			console.log("false");
-			this.addRecord(this.context.currentWordIndex, false);
-			return {status:false, translation:t_translation};
-		}
-	},
+    submitAnswer: function (user_translation, word_id) {
+        var t_translation = this.vocabularyData.words[this.context.currentWordIndex].translation;
+        var newCycle = false;
+        if (this.context.currentWordCount == this.vocabularyData.words.length - 1)
+            newCycle = true;
 
-	nextWord : function(){
-		if(this.context.currentWordCount<this.vocabularyData.words.length){
-			console.log("Loading next word.");
-			this.context.currentWordCount++;
-			this.context.currentWordIndex = this.currentShuffled[this.context.currentWordCount-1].id;
+        if (t_translation == user_translation) {
+            console.log("true");
+            this.addRecord(this.context.currentWordIndex, true);
+            return {
+                status: true,
+                newCycle: newCycle
+            }
+        } else {
+            console.log("false");
+            this.addRecord(this.context.currentWordIndex, false);
+            console.log("WRONG");
+            console.log('currentWordIndex : ', this.context.currentWordIndex);
+            console.log(this.vocabularyData.words[this.context.currentWordIndex]);
+            return {
+                status: false,
+                translation: t_translation,
+                newCycle: newCycle
+            };
+        }
+    },
 
-			return {status:true, data:this.currentShuffled[this.context.currentWordCount-1]};
-		} else {
-			if(this.context.currentCycle<this.options.trainingCycles){
-				console.log("Starting next cycle");
-				this.context.currentCycle++;
-				this.context.currentWordCount = 0;
-				this.context.currentWordIndex = 0;
-				this.shuffleNewBatch();
-				return {status:true, data:this.currentShuffled[this.context.currentWordCount]};
-			} else {
-				console.log("Finished!");
-				return {status:false};
-			}
-		}
-	},
+    nextWord: function () {
+    	console.log('CURRENT WORD COUNT ', this.context.currentWordCount);
+        if (this.context.currentWordCount < this.vocabularyData.words.length-1) {
+        	this.context.currentWordCount++;
+            console.log("Loading next word.");
+            this.context.currentWordIndex = this.currentShuffled[this.context.currentWordCount].id;
+            
+            return {
+                status: true,
+                data: this.currentShuffled[this.context.currentWordCount]
+            };
+        } else {
+            if (this.context.currentCycle < this.options.trainingCycles) {
+                console.log("Starting next cycle");
+                this.context.currentCycle++;
+                this.context.currentWordCount = 0;
+                this.shuffleNewBatch();
+                this.context.currentWordIndex = this.currentShuffled[0].id;
+       
+                return {
+                    status: true,
+                    data: this.currentShuffled[0]
+                };
+            } else {
+                console.log("Finished!");
+                return {
+                    status: false
+                };
+            }
+        }
+    },
 
-	addRecord : function(word_id, status){
-		this.stats[this.context.currentCycle].push({word_id:word_id, status:status});
-		console.log(this.stats);
-	},
+    addRecord: function (word_id, status) {
+        this.stats[this.context.currentCycle].push({
+            word_id: word_id,
+            status: status
+        });
+    },
 
-	generateStatistics : function(){
-		var statistics = {};
+    generateStatistics: function () {
+        var statistics = false;
+     
 
-		/* 1. generate current cycle progress */
-		var cycleProgress = [];
-		for(var i=0; i<this.context.currentCycle; i++){
-			var cycle = this.stats[i];
-			var cycleSum =0;
-			var cycleTotal = this.stats[i].length;
-			for(var j=0; j<cycleTotal; j++){ // those funny  ? : ? :
-				if(this.stats[i][j].status)
-					cycleSum++;
-			}
-			cycleProgress.push(cycleSum/cycleTotal);
-		}
-		console.log(cycleProgress);
-	}
+        /* 1. generate current cycle progress */
+        var cyclesProgress = [];
+        for (var i = 0; i < this.context.currentCycle+1; i++) {
+            var cycle = this.stats[i];
+            var cycleSum = 0;
+            var cycleTotal = this.stats[i].length;
+
+            for (var j = 0; j < cycleTotal; j++) { // those funny  ? : ? :
+                if (this.stats[i][j].status)
+                    cycleSum++;
+            }
+
+            cyclesProgress.push(cycleSum / cycleTotal);
+        }
+
+        statistics = {'cyclesProgress' : cyclesProgress};
+
+        return statistics;
+    }
 
 }
-shuffle = function(o){
-			for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-			return o;
-		};
+shuffle = function (o) {
+    for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+};
